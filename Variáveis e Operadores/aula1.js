ce52f163e18c2017bba4125f787e17bc3447c87f
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-11-12 22:19:52 -0300

    Primeiras aulas

--- Variáveis e Operadores/aula1.js
+++ Variáveis e Operadores/aula1.js
@@ -1,3 +1,5 @@
+//Console e Como Declarar Variáveis
+
 let variavel1 = 5; //Variável que muda
 const variavel2 = 10; //Variável constante
 
---
+++ Variáveis e Operadores/aula2.js
@@ -0,0 +1,8 @@
+//Diferenças entre Variáveis e Tipos de Operadores
+
+let atribuicao = 5; //O símbolo de = atribui um valor à variável
+
+const adicao = 5 + 5;
+
+console.log(adicao);
+//Operadores matemáticos: +(adição), -(subtração), *(multiplicação) e /(divisão)
