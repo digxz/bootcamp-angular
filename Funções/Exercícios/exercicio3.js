0ba2243790cd9121deee7822df634dbbc3729e0f
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-11-20 23:36:23 -0300

    +1 exercício de funções

---
+++ Funções/Exercícios/exercicio3.js
@@ -0,0 +1,7 @@
+//Escreva uma função que realize a conversão de uma temperatura fornecida em graus Fahrenheit (F) para Celsius (C).
+
+function conversao(temperatura) {
+  console.log((temperatura - 32) * 0.5556);
+}
+
+conversao(50);
