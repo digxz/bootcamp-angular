593da1b572a239249bd72c22eb11fd7744c20c15
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-11-21 21:46:51 -0300

    Início de objetos

---
+++ Classes e Objetos/Objetos/aula1.js
@@ -0,0 +1,12 @@
+//Estrutura básica de um objeto
+
+var digs = {
+  nome: "diego",
+  idade: 18,
+};
+
+//O Objeto é uma coleção de chaves e valores.
+//No exemplo acima, o objeto digs possui as chaves 'nome' e 'idade'
+//Os valores são "diego" e 18
+
+console.log(digs);
