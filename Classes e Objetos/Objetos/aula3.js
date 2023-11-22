ec903dbf3e70eaeea41d60cc6dde9d201e902cc6
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-11-21 22:23:35 -0300

    Problema resolvido

--- Classes e Objetos/Objetos/aula3.js
+++ Classes e Objetos/Objetos/aula3.js
@@ -2,13 +2,20 @@
 
 //Um método é uma função dentro de um objeto
 
-const digs = {
+var digs = {
   nome: "diego",
   idade: 18,
 
   descrever: function () {
-    console.log("Meu nome é ${this.nome} e minha idade é ${this.idade}");
+    console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade} anos!`);
   },
 };
+
+//As backsticks (´´) são usadas para criar template strings em JavaScript.
+//As template strings são strings que permitem incorporar expressões e variáveis
+//dentro delas de uma maneira mais conveniente e legível do que as strings tradicionais
+//criadas com aspas simples ('') ou duplas ("").
+
+//O comando this diz respeito ao contexto da função, como essa função está dentro do objeto, o this assume o objeto.
 
 digs.descrever();
