2aada97e9586cfca37204581bc3172982aed3d9a
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-11-21 23:07:28 -0300

    Classes - Início

--- Classes e Objetos/Objetos/aula4.js
+++ Classes e Objetos/Objetos/aula4.js
@@ -1,1 +1,12 @@
 //Acessando dinamicamente valores de um objeto
+
+var digs = {
+  nome: "diego",
+  idade: 18,
+
+  descrever: function () {
+    console.log(`Meu nome é ${this.nome} e minha idade é ${this.idade} anos!`);
+  },
+};
+
+console.log(digs["nome"]);
