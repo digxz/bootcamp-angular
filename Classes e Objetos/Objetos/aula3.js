6326e6127ec8c0a323fe775e75f02ebdd85b8270
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-11-21 22:15:24 -0300

    Problema - aula3.js

--- Classes e Objetos/Objetos/aula3.js
+++ Classes e Objetos/Objetos/aula3.js
@@ -1,1 +1,14 @@
 //Criando métodos para os objetos
+
+//Um método é uma função dentro de um objeto
+
+const digs = {
+  nome: "diego",
+  idade: 18,
+
+  descrever: function () {
+    console.log("Meu nome é ${this.nome} e minha idade é ${this.idade}");
+  },
+};
+
+digs.descrever();
