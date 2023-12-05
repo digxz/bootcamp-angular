fc90ebd8098c2fef92eddb4a867a02b476e13875
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-04 22:39:43 -0300

    Resolução ex Classes

--- Classes e Objetos/Exercícios/exercicio2.js
+++ Classes e Objetos/Exercícios/exercicio2.js
@@ -15,7 +15,29 @@ class Pessoa {
     this.altura = altura;
   }
 
-  imc(peso, altura) {
-    return peso / (altura * altura);
+  imc() {
+    return this.peso / (this.altura * this.altura);
+  }
+
+  classificarNivel() {
+    const imc = this.imc();
+
+    if (imc < 18.5) {
+      return "Abaixo do peso";
+    } else if (imc >= 18.5 && imc <= 25) {
+      return "Peso normal";
+    } else if (imc > 25 && imc <= 30) {
+      return "Acima do peso";
+    } else if (imc > 30 && imc <= 40) {
+      return "Obeso";
+    } else if (imc > 40) {
+      return "Obesidade Grave";
+    }
   }
 }
+
+let digs = new Pessoa("Diego", 65, 1.73);
+console.log(digs.classificarNivel());
+
+let bezinho = new Pessoa("Bernardo", 39, 1.5);
+console.log(bezinho.classificarNivel());
