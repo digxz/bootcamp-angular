2e3ef1a16ab4ff15659fb3d1fe1ca05beef48662
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-07 01:28:38 -0300

    Exercicio Extra - Classes

---
+++ Classes e Objetos/Exercícios/exercicioextra1.js
@@ -0,0 +1,23 @@
+//Fazer um sistema que indique o quanto uma pessoa ganha por hora
+
+class Pessoa {
+  nome;
+  salario;
+  horasPorDia;
+
+  constructor(nome, salario, horasPorDia) {
+    this.nome = nome;
+    this.salario = salario;
+    this.horasPorDia = horasPorDia;
+  }
+
+  calcularSalario() {
+    return this.salario / 22 / this.horasPorDia;
+  } //22 dias no mês
+}
+
+const digs = new Pessoa("Diego", 950, 4);
+console.log(digs.calcularSalario());
+
+const yas = new Pessoa("Yasmin", 5350, 4);
+console.log(yas.calcularSalario());
