b4ddbfa03b0dd8d0063c4c0bb85c130d48c0563b
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-09 02:43:03 -0300

    Continuação Arrays

---
+++ Arrays e Estruturas de Rep/Entendendo Arrays/aula3.js
@@ -0,0 +1,14 @@
+//Entendendo a necessidade de estruturas de repetição
+
+const notas = [];
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+
+notas;
+
+console.log(notas.length);
+// .length serve para saber quantos itens há na lista
