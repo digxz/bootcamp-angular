aed8008e889d8da13a4d324d7c3904ef2f6125cd
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-11 23:29:20 -0300

    Estruturas de repetição

---
+++ Arrays e Estruturas de Rep/Entendendo Estruturas de Rep/aula1.js
@@ -0,0 +1,8 @@
+//Como funciona a estrutura for
+
+for (let i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+//Enquanto i for menor que 10, o código será executado.
+//A última condição diz que será incrementado 1 a cada vez que o código roda
