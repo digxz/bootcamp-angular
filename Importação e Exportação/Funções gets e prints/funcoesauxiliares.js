f50b71a22c8a965d94b1b6494a0d806ae56dd335
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-14 19:42:46 -0300

    Importação e Exportação

---
+++ Importação e Exportação/Funções gets e prints/funcoesauxiliares.js
@@ -0,0 +1,16 @@
+//Criando a Função Gets e Print
+
+//gets e print são funções auxiliares
+
+function gets() {
+  return 10;
+}
+
+function print(texto) {
+  console.log(texto);
+}
+
+module.exports = {
+  gets,
+  print,
+};
---
+++ Importação e Exportação/Funções gets e prints/main.js
@@ -0,0 +1,3 @@
+const { gets, print } = require("./funcoesauxiliares");
+
+print(gets());
