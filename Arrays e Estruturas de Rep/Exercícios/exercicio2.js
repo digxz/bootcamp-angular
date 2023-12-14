abcf483203a74c40715eef2a03ead4a1a0b4b54c
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-13 22:41:24 -0300

    Fim de Arrays e Estruturas de Rep

--- Arrays e Estruturas de Rep/Exercícios/exercicio2.js
+++ Arrays e Estruturas de Rep/Exercícios/exercicio2.js
@@ -1,1 +1,11 @@
-//Apenas números pares
+//Apenas números pares de uma lista
+
+const listaDeNumeros = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (let i = 0; i < listaDeNumeros.length; i++) {
+  const numerosDaLista = listaDeNumeros[i];
+
+  if (numerosDaLista % 2 === 0) {
+    console.log(numerosDaLista);
+  }
+}
