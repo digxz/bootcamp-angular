858d227ffdfd0658db58f3d27b8a0deac73e0088
digxz <diegocastelliniramirez.dcr@gmail.com>
2023-12-14 21:44:55 -0300

    Exercício de Import e Export

--- Importação e Exportação/Funções gets e prints/funcoesauxiliares.js
+++ Importação e Exportação/Funções gets e prints/funcoesauxiliares.js
@@ -2,8 +2,13 @@
 
 //gets e print são funções auxiliares
 
+const conjuntoDeEntradas = [5, 50, 10, 98, 23];
+let i = 0;
+
 function gets() {
-  return 10;
+  const valor = conjuntoDeEntradas[i];
+  i++;
+  return valor;
 }
 
 function print(texto) {
--- Importação e Exportação/Funções gets e prints/main.js
+++ Importação e Exportação/Funções gets e prints/main.js
@@ -1,3 +1,19 @@
 const { gets, print } = require("./funcoesauxiliares");
 
-print(gets());
+const numerosDaLista = [];
+
+for (let i = 0; i < 5; i++) {
+  const numeroSorteado = gets();
+  numerosDaLista.push(numeroSorteado);
+}
+
+let maiorValor = 0;
+
+for (let i = 0; i < numerosDaLista.length; i++) {
+  const numeroSorteado = numerosDaLista[i];
+  if (numeroSorteado > maiorValor) {
+    maiorValor = numeroSorteado;
+  }
+}
+
+console.log(maiorValor);
